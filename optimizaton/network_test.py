eed77fc3b0207665e4249aaee796ab613eccdb02
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-06-02 15:56:41 -0500

    Gets mnist nn running - loss does not look correct

--- optimizaton/network_test.py
+++ optimizaton/network_test.py
@@ -23,9 +23,9 @@ n_epochs = 10
 
 # Layer 1
 l1_dim = (n_features, n_features)
-w1 = theano.shared(rng.normal(size=l1_dim)), name='w1')
+w1 = theano.shared(rng.normal(size=l1_dim), name='w1')
 b1 = theano.shared(0., name='b1')
-lf1 = T.nnet.relu(T.dot(x, w1) + b1)
+lf1 = T.nnet.relu(T.dot(x.T, w1) + b1)
 
 # Layer 2
 l2_dim = (n_features, n_features)
@@ -40,19 +40,19 @@ b3 = theano.shared(0., name='b3')
 lf3 = T.nnet.relu(T.dot(lf2, w3) + b3)
 
 # Output layer
-o_dim = (n_features, 2)
-w4 = theano.shared(rng.randn(2), name='w4')
+o_dim = (n_features, 10)
+w4 = theano.shared(rng.normal(size=o_dim), name='w4')
 b4 = theano.shared(0., name='b4')
 output = T.nnet.softmax(T.dot(lf3, w4) + b4)
 prediction = T.argmax(output, axis=1)
-loss = T.mean(T.nnet.binary_crossentropy())
+loss = T.mean(T.nnet.categorical_crossentropy(output, y))
 
 # Update rules
 # Since we have a number
 alpha = T.iscalar('alpha')
 param_updates = OrderedDict()
-for x in [w1, b1, w2, b2, w3, b3, w4, b4]:
-    param_updates[x] = x = alpha * T.grad(loss, x)
+for a in [w1, b1, w2, b2, w3, b3, w4, b4]:
+    param_updates[a] = a + alpha * T.grad(loss, a)
 
 
 
@@ -64,3 +64,13 @@ obj_fn = theano.function(inputs=[x, y, alpha],
 pred_fn = theano.function(inputs=[x],
                           outputs=prediction,
                           allow_input_downcast=True)
+
+log_interval = 1000
+for i in range(n_epochs):
+    count = 0
+    for (data_x, data_label) in zip(train_X, train_y):
+        cost = obj_fn(data_x.reshape((784,1)), data_label.reshape((1)), 0.01/numpy.sqrt(count+1+i*len(train_y)))
+        if count % log_interval == 0:
+            print("Cost at iteration {} epoch {} is {}".format(
+                count, i, cost))
+        count += 1
